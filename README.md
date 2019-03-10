3afa0a4777263ceaa52e75d1ed270f9b9cb50530
onikazu <programingmanagement@gmail.com>
2019-03-10 15:01:08 +0900

    add scraping project

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ javascript game library, enchant.js.
 these program is written based on bellow.
 
 [Qiita article url](https://qiita.com/ij_spitz/items/8c27ecc68b0bac9e8574#8-%E8%A1%9D%E7%AA%81%E5%88%A4%E5%AE%9A%E3%82%92%E3%81%97%E3%81%A6%E3%81%BF%E3%82%88%E3%81%86)
+
+### JavascriptProject
+these program is written based on bellow.
+
+[JavaScript初級者から中級者になろう](https://uhyohyo.net/javascript/)
+
+### ScrapingProject
+these program is written based on bellow.
+
+[Qiita article url](https://qiita.com/ritukiii/items/272d485e8a249d0d1bd7)
---
+++ ScrapingProject/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,60 @@
+import scrapy
+
+
+# $ scrapy crawl quotes
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-{}.html'.format(page)
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file {}'.format(filename))
+
+
+# $ scrapy crawl quotes_json -o result.json
+class QuotesJsonSpider(scrapy.Spider):
+    name = "quotes_json"
+
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract()
+            }
+
+
+class QuotesReccurentSpider(scrapy.Spider):
+    name = "quotes_reccurent"
+
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
